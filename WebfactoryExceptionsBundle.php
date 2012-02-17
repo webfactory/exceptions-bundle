f03084e737bb1c6448c7457c972b562d3e2a2dab
Per Bernhardt <plb@webfactory.de>
2012-02-17 13:43:35 -0100

    Bugfix

--- WebfactoryExceptionsBundle.php
+++ WebfactoryExceptionsBundle.php
@@ -3,6 +3,7 @@
 namespace Webfactory\Bundle\ExceptionsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class WebfactoryExceptionsBundle extends Bundle {
 
