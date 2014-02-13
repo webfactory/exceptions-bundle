5dc76bd3f6e37347a89d266cb02bbb5eb3eed8b6
Unknown <unknown@kiln.example.com>
2014-02-13 13:53:15 -0100

    Add license information and fix composer.json file

--- Controller/ExceptionController.php
+++ Controller/ExceptionController.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * (c) webfactory GmbH <info@webfactory.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Webfactory\Bundle\ExceptionsBundle\Controller;
 
 use Symfony\Bundle\TwigBundle\Controller\ExceptionController as BaseController;
