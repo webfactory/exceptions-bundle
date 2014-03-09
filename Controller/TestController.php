80120fdd91f9f3e40b911f64598705aaa3089435
Ryan Weaver <ryan@thatsquality.com>
2014-03-09 17:27:28 -0500

    Removing duplicate route - we only need one route, which a nicely-placed default for _format

--- Controller/TestController.php
+++ Controller/TestController.php
@@ -15,8 +15,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class TestController extends Controller {
 
     /**
-     * @Route("/{code}/", defaults={"_format" = "html"}, requirements={"code" = "\d+"})
-     * @Route("/{code}/{_format}/", requirements={"code" = "\d+"})
+     * @Route("/{code}/{_format}", defaults={"_format" = "html"}, requirements={"code" = "\d+"})
      */
     public function testErrorPageAction($code) {
         $exceptionController = $this->get('twig.controller.exception');
